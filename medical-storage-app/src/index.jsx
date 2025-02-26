b9c14dc645ab95c0c1ca57de80db6b614c730712
Dorerishi <hrushikeshraghvendra@gmail.com>
2025-02-26 12:46:35 +0530

    testing frontend

--- medical-storage-app/src/index.jsx
+++ medical-storage-app/src/index.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 
-import ReactDOM from 'react/client';
+import ReactDOM from 'react-dom/client';
+
+const root_element= document.getElementById('root');
+
+const root = ReactDOM.createRoot(root_element);
+
+root.render(<div>Hello there</div>);
+
 
